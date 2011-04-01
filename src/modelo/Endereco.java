57977d8a0383886faa6515f178474c394f73611d
adroaldo.borges <adroaldo.borges@bacana.cec.ime.usp.br>
2011-04-01 12:14:30 -0300

    Classe endereço mais organizado.

--- src/modelo/Endereco.java
+++ src/modelo/Endereco.java
@@ -1,23 +1,100 @@
 package modelo;
 
+import javax.persistence.Column;
 import javax.persistence.Embedded;
 import javax.persistence.Entity;
-import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
 
 @Entity
-@Table(name = "endereco")
+@Table(name = "Endereco")
 public class Endereco {
 
+	@Id
+	@GeneratedValue
+	private Long enderecoID;
+	@Column
 	private String logadouro;
+	@Column
 	private String numero;
+	@Column
 	private String complemento;
+	@Column
 	private String cidade;
+	@Column
 	private String estado;
+	@Column
 	private String pais;
-	private String cep;
+	@OneToOne
+	private CEP cep;
 
 	@Embedded
 	private Telefone telefones;
+
+	public String getLogadouro() {
+		return logadouro;
+	}
+
+	public void setLogadouro(String logadouro) {
+		this.logadouro = logadouro;
+	}
+
+	public String getNumero() {
+		return numero;
+	}
+
+	public void setNumero(String numero) {
+		this.numero = numero;
+	}
+
+	public String getComplemento() {
+		return complemento;
+	}
+
+	public void setComplemento(String complemento) {
+		this.complemento = complemento;
+	}
+
+	public String getCidade() {
+		return cidade;
+	}
+
+	public void setCidade(String cidade) {
+		this.cidade = cidade;
+	}
+
+	public String getEstado() {
+		return estado;
+	}
+
+	public void setEstado(String estado) {
+		this.estado = estado;
+	}
+
+	public String getPais() {
+		return pais;
+	}
+
+	public void setPais(String pais) {
+		this.pais = pais;
+	}
+
+	public CEP getCep() {
+		return cep;
+	}
+
+	public void setCep(CEP cep) {
+		this.cep = cep;
+	}
+
+	public Telefone getTelefones() {
+		return telefones;
+	}
+
+	public void setTelefones(Telefone telefones) {
+		this.telefones = telefones;
+	}
 }
