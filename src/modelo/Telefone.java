1d612216de9db6e818b45098167fa12622f9ba6d
adroaldo.borges <adroaldo.borges@bacana.cec.ime.usp.br>
2011-04-01 13:23:17 -0300

    Adicionando a classe telefone.

---
+++ src/modelo/Telefone.java
@@ -0,0 +1,27 @@
+package modelo;
+
+import java.io.Serializable;
+
+import javax.persistence.Embeddable;
+import javax.persistence.Entity;
+
+@Embeddable
+@Entity
+public class Telefone {
+
+	private String residencial;
+	private String comercial;
+	
+	public String getResidencial() {
+		return residencial;
+	}
+	public void setResidencial(String residencial) {
+		this.residencial = residencial;
+	}
+	public String getComercial() {
+		return comercial;
+	}
+	public void setComercial(String comercial) {
+		this.comercial = comercial;
+	}
+}
