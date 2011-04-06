9a7dfd91ed1012908302848168f0c62a47a045f9
adroaldo.borges <adroaldo.borges@bacana.cec.ime.usp.br>
2011-04-06 14:29:12 -0300

    Adicionando

---
+++ src/br/usp/ime/ingpos/modelo/CedulaDeIdentidade.java
@@ -0,0 +1,29 @@
+package br.usp.ime.ingpos.modelo;
+
+import javax.persistence.Entity;
+
+@Entity
+public enum CedulaDeIdentidade {
+	RG("", ""), RNE("", ""), PASSAPORTE("","");
+	
+	private String numero;
+	private String digito;
+	
+	CedulaDeIdentidade(String numero, String digito) {
+		this.numero = numero;
+		this.digito = digito;
+	}
+	public String getNumero() {
+		return numero;
+	}
+	public void setNumero(String numero) {
+		this.numero = numero;
+	}
+	
+	public String getDigito() {
+		return digito;
+	}
+	public void setDigito(String digito) {
+		this.digito = digito;
+	}
+}
